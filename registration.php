d5503c98af1198a15fde0f52dae7c425818f07f1
Nidhi Gupta <nidhigupta01031995@gmail.com>
2020-04-02 12:10:30 +0530

    add code

---
+++ registration.php
@@ -0,0 +1,6 @@
+<?php
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Salecto2_Magento2FrontendTheme',
+    __DIR__
+);
